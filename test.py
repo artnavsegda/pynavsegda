e96f7eef1a2272ef772b65a6456be0d5dd965460
Art Navsegda <artnavsegda@gmail.com>
2020-11-03 11:27:34 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("test")
+def my_function(cip_join):
+    packed_join = (cip_join // 256) + ((cip_join % 256) * 256)
+    print(packed_join)
+
+my_function(25)
+my_function(1000)
+my_function(15000)
